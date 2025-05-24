699e0ec6cec7d2bd8bdc8ad5f411b90125f4779d
ksiscute <thebeast47yt@gmail.com>
2025-05-24 12:55:13 -0400

    first

---
+++ bronxbot.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
+
+bot.run('TOKEN')
