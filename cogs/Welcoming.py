f6cfb730809c50fc4e52a5b39529853124e00fe4
ksiscute <thebeast47yt@gmail.com>
2025-05-25 13:59:53 -0400

    re-added the funny custom welcome message with easier adjustability & testing

--- cogs/Welcoming.py
+++ cogs/Welcoming.py
@@ -15,6 +15,18 @@ logging.basicConfig(
 )
 logger = logging.getLogger('Welcoming')
 
+async def welcomeEmbed(member):
+    with open('data/messages.json', 'r') as f:
+        data = json.load(f)
+    character = random.choice(data['characters'])
+    welcomeEmbed = discord.Embed(
+        description=f"\"{random.choice(data['messages'])}\"\n-# [Main Server](https://discord.gg/furryporn) |  [Backup Server](https://discord.gg/W563EnFwed) | [Appeal Server](https://discord.gg/6Th9dsw6rM)",
+        color=discord.Color.random()
+    )
+    welcomeEmbed.set_author(name=character.title(), icon_url=data['character'][character], url="https://discord.gg/6Th9dsw6rM")
+    welcomeEmbed.set_footer(text="click the title to join the appeal server if you get banned", icon_url=member.guild.icon.url)
+    return welcomeEmbed
+
 class Welcoming(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -32,18 +44,17 @@ async def on_member_join(self, member):
     if member.guild.id == 1259717095382319215:
         channel = member.guild.get_channel(1368768246475391037)
         await channel.send(f"{random.choice(greeting)} {member.mention} {randomEmoji}")
-        embed = discord.Embed(
-            description="welcome to South Bronx...\n-# [Main Server](https://discord.gg/furryporn) |  [Backup Server](https://discord.gg/W563EnFwed) | [Appeal Server](https://discord.gg/6Th9dsw6rM)",
-            color=discord.Color.random()
-        )
-        embed.set_author(name="Thanks for joining!", icon_url=member.avatar.url, url="https://discord.gg/6Th9dsw6rM")
-        embed.set_footer(text="click the title to join the appeal server if you get banned", icon_url=member.guild.icon.url)
+        embed = welcomeEmbed
         await member.send(embed=embed)
         with open("data/stats.json", "r") as f:
             data = json.load(f)
             data["stats"][str(member.guild.id)]["gained"] += 1
             with open("data/stats.json", "w") as f:
                 json.dump(data, f, indent=2)
+
+@commands.command()
+async def welcometest(self, ctx):
+    await ctx.author.send(embed=welcomeEmbed(ctx.author))
 
 @commands.Cog.listener()
 async def on_member_remove(self, member):
