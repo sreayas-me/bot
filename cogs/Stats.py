a4e39f6bfca962ff21bfc8676b9a47d007327d72
ksiscute <thebeast47yt@gmail.com>
2025-05-25 00:04:04 -0400

    made stats specific to server

--- cogs/Stats.py
+++ cogs/Stats.py
@@ -15,7 +15,7 @@ logging.basicConfig(
     ]
 )
 logger = logging.getLogger('Stats')
-
+guilds = [1259717095382319215, 1299747094449623111, 1142088882222022786]
 class Stats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -25,11 +25,12 @@ class Stats(commands.Cog):
     async def on_message(self, message):
         if message.author.bot:
             return
-        with open("data/stats.json", "r") as f:
-            data = json.load(f)
-            data["stats"][str(message.guild.id)]["messages"] += 1
-        with open("data/stats.json", "w") as f:
-            json.dump(data, f, indent=2)
+        if message.guild.id in guilds:
+            with open("data/stats.json", "r") as f:
+                data = json.load(f)
+                data["stats"][str(message.guild.id)]["messages"] += 1
+            with open("data/stats.json", "w") as f:
+                json.dump(data, f, indent=2)
 
 
     @commands.command(name="stats", aliases=["st"])
