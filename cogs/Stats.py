b76030cbb1791fcb815a061d2a24e3af208d3347
ksiscute <thebeast47yt@gmail.com>
2025-05-24 20:06:54 -0400

    functional stats

--- cogs/Stats.py
+++ cogs/Stats.py
@@ -2,6 +2,7 @@ import discord
 import json
 from discord.ext import commands
 import logging
+import datetime
 
 # Set up logging
 logging.basicConfig(
@@ -33,7 +34,27 @@ class Stats(commands.Cog):
     @commands.command(name="stats", aliases=["st"])
     @commands.is_owner()
     async def stats(self, ctx):
-        await ctx.send("```json\n" + json.dumps(self.bot.stats, indent=2) + "```")
+        with open("data/stats.json", "r") as f:
+            data = json.load(f)
+        embed = discord.Embed(
+            description=f"""
+            `messages {data["messages"]}`
+            `members gained {data["gained"]}`
+            `members lost {data["lost"]}`
+            `total g/l {data["gained"] - data["lost"]}`
+            """,
+            color=discord.Color.random()
+        )
+        embed.set_thumbnail(url=self.bot.user.avatar.url)
+        embed.set_footer(text="stats since " + data["timestamp"].split("T")[0], icon_url=ctx.guild.icon.url)
+        await ctx.reply(embed=embed)
+
+    @commands.command(name="resetstats", aliases=["rst"])
+    @commands.is_owner()
+    async def resetstats(self, ctx):
+        with open("data/stats.json", "w") as f:
+            json.dump({"gained": 0, "lost": 0, "messages": 0, "timestamp": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")}, f, indent=2)
+        await ctx.reply("Stats have been reset!")
 
 async def setup(bot):
     try:
